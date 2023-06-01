da2c588cf615d5cb5b83ebcd362730bf41554075
Alexandr Noskov <san9nosko8@gmail.com>
2023-06-01 21:21:45 +0300

    1.def get_service(cls) -> build: 2.def to_json(self, filename: str) -> None:

--- src/channel.py
+++ src/channel.py
@@ -26,7 +26,7 @@ class Channel:
 
     @classmethod
     def get_service(cls) -> build:
-        """Возвращает объект для работы с API youtube."""
+        """Возвращает объект для работы с API youtub."""
         service = build('youtube', 'v3', developerKey=cls.__API_KEY)
         return service
 
