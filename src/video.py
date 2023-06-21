23756b6d70c905aba9aa7faf054575fcdeb0ea11
Alexandr Noskov <san9nosko8@gmail.com>
2023-06-21 23:15:42 +0300

    Класс Video try/except

--- src/video.py
+++ src/video.py
@@ -2,21 +2,26 @@ import os
 from googleapiclient.discovery import build
 import json
 
+from helper.youtube_api_manual import youtube
+
+
 class Video:
     def __init__(self, video_id: str):
         self.id_video = video_id
         try:
-            video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                      id=video_id).execute()
-            self.title: str = video_response['items'][0]['snippet']['title']
-            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
-            self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+
+            self.video_info = youtube.get_video(video_id)
+            self.video_id = self.video_info['items'][0]['id']
+            self.video_title = self.video_info['items'][0]['snippet']['title']
+            self.viewCount = self.video_info['items'][0]['statistics']['viewCount']
+            self.likeCount = self.video_info['items'][0]['statistics']['likeCount']
         except IndexError:
-            self.title = None
-            self.view_count = None
-            self.like_count = None
-            self.comment_count = None
+            self.video_id = video_id
+            self.video_info = None
+            self.video_title = None
+            self.viewCount = None
+            self.likeCount = None
+            print(f"Видеоролик с id {self.video_id} не найден/не существует")
 
     def __str__(self):
         return self.title
